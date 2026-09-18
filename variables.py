2aa5d34c9e44c88951040396792ce5532d8273d5
Ximena Callejas <xcallejasg@gmail.com>
2026-09-17 23:36:44 -0600

    Commit 2: trabajando con variables

--- variables.py
+++ variables.py
@@ -1,4 +1,8 @@
 # Esta sesión será para aprender el uso de variables 
-print ("Hola Mundo!")
-print ("Hoy es miércoles")
-print ("Mañana es jueves")
+print ("Sistema de mensajería")
+distancia_km = 6.5
+peso_kg = 2.0
+es_urgente = True
+Zona = "B"
+distancia_km = distancia_km + 16
+print (distancia_km)
